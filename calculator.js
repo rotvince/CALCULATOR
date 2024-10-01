94d434cfdf551ac803a6045787a3a817c26cc702
rotvince <ngeno2001@gmail.com>
2024-10-01 10:22:08 +0300

    first commit

---
+++ calculator.js
@@ -0,0 +1,11 @@
+const  keys = document.querySelectorAll('.key');
+const display_input = document.querySelector('.display .input')
+const display_output = document.querySelector('.display .output')
+
+let input = "";
+array.forEach (let key of keys) { 
+    const value = key.dataset.key;
+    key.addEventlistener('click', () => {
+    if (value == "clear")
+});
+}
